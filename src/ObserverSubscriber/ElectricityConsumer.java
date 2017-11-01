efe8180fdeffe5d03f8a6310881594f408c3cf08
Evgeny Shumakov <shumakov.e@gmail.com>
2017-11-01 15:24:50 +0300

    Commented

--- src/ObserverSubscriber/ElectricityConsumer.java
+++ src/ObserverSubscriber/ElectricityConsumer.java
@@ -4,5 +4,9 @@ package ObserverSubscriber;
  * Created by shumakove on 30.10.17.
  */
 public interface ElectricityConsumer {
+    /**
+     * This must be implemented all consumers(Tv,Lamp)
+     * it used for notification when PowerSwitch is powered On
+     */
     public void powerOn();
 }
